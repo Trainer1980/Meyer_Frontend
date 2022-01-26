f8a19309453b75297a20041e24dea79c84d9a6e0
Holly Howell <hollyhowell1980@gmail.com>
2022-01-25 23:30:07 -0600

    Modal complete

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,76 +1,53 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Grid, Button } from '@mui/material';
+import { Grid } from '@mui/material';
 import ProductModal from './ProductModal';
 
-const ProductList = () => {
+const ProductList = (props) => {
   const [products, setProducts] = useState([]);
-  const [open, setOpen] = useState(false)
-  const [prod, setProd] = useState()
-  const data = {
-    prod
-  }
-  //const [modalIsOpen, setIsOpen] = useState(false);
 
   useEffect(() => { axios.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline")
     .then((response) => setProducts(response.data))
     .catch(err => console.log("Error: ", err))
   })
 
-  const handleOpen = (e) => {
-    e.preventDefault();
-    let value = e.target.value;
-    axios.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline?id="+value)
-    .then((response) => setProd(response.data))
-    console.log(prod)
-    .catch(err => console.log("Error: ", err));
+  const [open, setOpen] = useState(false);
+
+  const [prod, setProd] = useState([]);
+
+  const handleOpen = (props) => {
+    setProd(props);
     setOpen(true);
-    }
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
-  // const openFromParent = () => {
-  //   setIsOpen(true);
-  // }
+  // let prod = products.map((product) => (
+  //   product.name,
+  //   product.price,
+  //   product.rating,
+  //   product.image_link,
+  //   product.description,
+  //   product.product_link,
+  //   product.product_colors
+  // ))
 
-  // const handleCloseModal = (e, data) => {
-  //   console.log(e, data);
-  //   setIsOpen(false);
-  // }
-
-  // const handleAfterOpen = (e, data) => {
-  //   console.log(e, data);
-  // }
   return (
     <div>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {products.map((product) => (
         <Grid item xs={2} sm={4} md={4} key={product.id}>
               <h3>{product.name}</h3>
-              <h4>Price: {product.price}</h4>
+              <h4>Price: ${product.price}</h4>
               <h4>Rating: {product.rating == null ? 'No rating': (product.rating)}</h4>
               <img src={product.image_link} alt='product'/>
-              <Button className='button' variant='contained' color='primary' value = {product.id} onClick = { handleOpen }>View More</Button>
+              <button value={product} onClick={() => handleOpen(product)}>View More</button>
         </Grid>
         ))}
       </Grid>
-      <ProductModal
-        props = {data}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      />
-      {/*       
-      <ProductModal
-        dynData={props}
-        IsModalOpened={modalIsOpen}
-        onCloseModal={handleCloseModal}
-        onAfterOpen={handleAfterOpen}
-      />
-       */}
+      <ProductModal open={open} onClose={handleClose} prod={prod} />
     </div>
   );
 }
--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -1,34 +1,49 @@
 import React from 'react';
-import { Modal, Typography, Box } from '@mui/material';
+import { Button, Modal, Box } from '@mui/material';
 
 
 const ProductModal = (props) => {
+    // console.log(props)
+    
     const contentStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: 400,
-      bgcolor: 'background.paper',
-      border: '2px solid #000',
-      boxShadow: 24,
-      p: 4,
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
     };
+    
+    let url = props.prod.product_link
+    
+    const prodWeb = () => {
+        window.open(url, "_blank")
+    }
     
 
     return (
-        <div>
-            <Modal>
-            <Box sx={contentStyle}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                     {props.name}
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
-            </Box>
-            </Modal>
-        </div>
+        <Modal
+            {...props}
+        >
+        <Box sx={contentStyle}>
+            <h2>{props.prod.name}</h2>
+            <img src={props.prod.image_link} alt='product'/>
+            <h4> Description: {props.prod.description} </h4>
+            <h4> Rating: {props.prod.rating == null ? 'No rating': (props.rating)} </h4>
+            <h4> Price: ${props.prod.price} </h4>
+            <h4> Colors offered: </h4>
+            {props.prod.product_colors != null && 
+            props.prod.product_colors.map(color =>
+                <div key={color.hex_value}>
+                    <ul>{color.colour_name} </ul>
+                </div>
+            )}
+            <Button variant='contained' color='primary' onClick={prodWeb}>Product Website</Button>
+        </Box>
+        </Modal>
     )
 }
 
