0a1ce6afb8ac48452e33f8200cc8ea375382d1d6
Holly Howell <hollyhowell1980@gmail.com>
2022-01-26 08:46:10 -0600

    Started Filters

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Grid } from '@mui/material';
+import { Grid, Checkbox, FormControlLabel, FormGroup, FormLabel, Box } from '@mui/material';
 import ProductModal from './ProductModal';
 
 const ProductList = (props) => {
@@ -24,18 +24,143 @@ const ProductList = (props) => {
     setOpen(false);
   };
 
-  // let prod = products.map((product) => (
-  //   product.name,
-  //   product.price,
-  //   product.rating,
-  //   product.image_link,
-  //   product.description,
-  //   product.product_link,
-  //   product.product_colors
-  // ))
-
   return (
     <div>
+      <div>
+          <h1>Maybelline Make-up</h1>
+      </div>
+      <div>
+          <h2>Filters</h2>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <Grid item xs={2} sm={4} md={4}>
+              <Box>
+                <FormGroup sx={{display: 'inline', width:'200px'}}>
+                  <h4>Rating:</h4>
+                  <FormControlLabel
+                    label="5"
+                    control={
+                      <Checkbox />
+                    } 
+                    />
+                  <FormControlLabel
+                  label="4-4.9"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="3-3.9"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="2-2.9"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="1-1.9"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="0-0.9"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="No Rating"
+                  control={
+                    <Checkbox />
+                  } />
+                </FormGroup>
+              </Box>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Box>
+                <FormGroup sx={{display: 'inline', width:'300px'}}>
+                  <h4>Price:</h4>
+                  <FormControlLabel
+                    label="$16+"
+                    control={
+                      <Checkbox />
+                    } />
+                  <FormControlLabel
+                  label="$13-15.99"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="$9-11.99"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="$5-7.99"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="4.99 and below"
+                  control={
+                    <Checkbox />
+                  } />
+                </FormGroup>
+              </Box>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Box>
+                <FormGroup sx={{display: 'inline', width:'400px'}}>
+                  <h4>Product Type:</h4>
+                  <FormControlLabel
+                    label="Blush"
+                    control={
+                      <Checkbox />
+                    } />
+                  <FormControlLabel
+                  label="Bronzer"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="Eyeliner"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="Eyeshadow"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="Foundation"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="Lip Liner"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="Lipstick"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="Mascara"
+                  control={
+                    <Checkbox />
+                  } />
+                  <FormControlLabel
+                  label="Nail Polish"
+                  control={
+                    <Checkbox />
+                  } />
+                </FormGroup>
+              </Box>
+            </Grid>
+          </Grid>
+      </div>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {products.map((product) => (
         <Grid item xs={2} sm={4} md={4} key={product.id}>
--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -3,7 +3,6 @@ import { Button, Modal, Box } from '@mui/material';
 
 
 const ProductModal = (props) => {
-    // console.log(props)
     
     const contentStyle = {
         position: 'absolute',
@@ -32,7 +31,7 @@ const ProductModal = (props) => {
             <h2>{props.prod.name}</h2>
             <img src={props.prod.image_link} alt='product'/>
             <h4> Description: {props.prod.description} </h4>
-            <h4> Rating: {props.prod.rating == null ? 'No rating': (props.rating)} </h4>
+            <h4> Rating: {props.prod.rating == null ? 'No rating': (props.prod.rating)} </h4>
             <h4> Price: ${props.prod.price} </h4>
             <h4> Colors offered: </h4>
             {props.prod.product_colors != null && 
